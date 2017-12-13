12a2750310c935f9ce3fc49ac0ca46802732d8e7
raymondtay <174844Q@mymail.nyp.edu.sg>
2017-12-14 00:17:04 +0800

    updated cart, item page, added reviews & comments function

--- models.py
+++ models.py
@@ -6,14 +6,24 @@ class Item(db.Model):
     name = db.Column(db.String(50))
     info = db.Column(db.String(50))
     price = db.Column(db.Float)
-    description = db.Column(db.Text(50))
+    description = db.Column(db.Text)
     calories = db.Column(db.Integer)
     category = db.Column(db.String(50))
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('Item.id'))
+    item_id = db.Column(db.Integer, unique=True)
+    name = db.Column(db.Text)
     quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    subtotal = db.Column(db.Float)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer)
+    name = db.Column(db.Text)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.Text)
 
 class ItemView(ModelView):
         form_choices = {
@@ -25,4 +35,6 @@ class ItemView(ModelView):
             ]
         }
 admin.add_view(ItemView(Item, db.session))
+admin.add_view(ModelView(Cart, db.session))
+admin.add_view(ModelView(Comments, db.session))
 
--- views.py
+++ views.py
@@ -1,6 +1,23 @@
 from OOPP import app, db
-from models import Item, Cart
+from models import Item, Cart, Comments
 from flask import render_template, redirect, url_for, request
+
+@app.context_processor
+def utility_processor():
+    def show_cart_price():
+        price = 0
+        cart = Cart.query.all()
+        for c in cart:
+            price += c.subtotal
+
+        return "{0:.2f}".format(price)
+    def cart_count():
+        count = 0
+        cart = Cart.query.all()
+        for c in cart:
+            count += 1
+        return count
+    return dict(show_cart_price=show_cart_price, cart_count=cart_count)
 
 @app.route('/')
 def index():
@@ -10,7 +27,52 @@ def index():
 def shop():
     items = Item.query.all()
 
-    return render_template("testshop.html", items=items)
+    return render_template("shop.html", items=items)
+
+@app.route('/shop/<int:item_id>/add')
+def addCart(item_id):
+
+    item1 = Item.query.filter_by(id=item_id).first()
+    quantity = 1
+    subtotal = item1.price
+    cart = Cart(item_id=item_id, name=item1.name, quantity=quantity, price=item1.price, subtotal=subtotal)
+    db.session.add(cart)
+    db.session.commit()
+
+    # check = Cart.query.filter(item_id == item_id).all()
+    # cartid = Cart.query.filter_by(item_id=item_id).first()
+    # if check is None:
+    #     item1 = Item.query.filter_by(id=item_id).first()
+    #     quantity = 1
+    #     subtotal = item1.price
+    #     cart = Cart(item_id=item_id, name=item1.name, quantity=quantity, price=item1.price, subtotal=subtotal)
+    #     db.session.add(cart)
+    #     db.session.commit()
+    # else:
+    #     cartitem = Cart.query.filter_by(name=cartid.name).first()
+    #     cartitem.quantity += 1
+    #     cartitem.subtotal = cartitem.price * cartitem.quantity
+    #     db.session.commit()
+
+    return redirect(url_for('shop'))
+
+@app.route('/cart')
+def cart():
+    cart = Cart.query.all()
+
+    return render_template("cart.html", cart=cart)
+
+# @app.route('/cart/update/<int:item_id>')
+# def update(item_id):
+#     cartid = Cart.query.filter_by(item_id=item_id).first()
+#     i = cartid.quantity
+#
+#
+#     for c in Cart:
+#         cartid = Cart.query.filter_by(item_id=item_id).first()
+#
+#
+#     return render_template("cart.html")
 
 @app.route('/adminadd')
 def adminadd():
@@ -23,6 +85,7 @@ def addItem():
     price = request.form['price']
     description = request.form['description']
 
+
     item = Item(name=name, info=info, price=price, description=description)
 
     db.session.add(item)
@@ -30,24 +93,38 @@ def addItem():
 
     return redirect(url_for('shop'))
 
+# @app.route('/view/<id>')
+# def viewitem(id):
+#      item =Cart.query.filter_by(itemid=id).first()
+#      itemname = item.name
+#      render template(template.html, itemname=item)
+
+
 @app.route('/item/<int:item_id>')
 def item(item_id):
     item = Item.query.filter_by(id=item_id).one()
 
-    return render_template('item.html', item=item)
+    comment = Comments.query.filter(Comments.item_id == item_id).all()
+
+    return render_template('item.html', item=item, comment=comment)
+
+@app.route('/item/<int:item_id>/add', methods=['POST'])
+def addComment(item_id):
+
+    name = request.form['name']
+    rating = request.form['rating']
+    comment = request.form['comment']
+
+    addComment = Comments(item_id=item_id, name=name, rating=rating, comment=comment)
+    db.session.add(addComment)
+    db.session.commit()
+
+    return redirect(url_for('item', item_id=item_id))
 
 @app.route('/adminview')
 def adminview():
     return render_template("adminview.html")
 
-@app.route('/cart')
-def cart():
-    return render_template("cart.html")
-
-@app.route('/addCart')
-def addCart():
-    pass
-	
 @app.route('/test')
 def test_cart():
      return "test placeholder."

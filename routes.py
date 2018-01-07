a1f514784e158995dad9abcf7c5f0f69985f0908
ThreeSugar <threesugar@hotmail.com>
2018-01-07 23:30:46 +0800

    deleted to make the heroku deployment work

--- routes.py
+++ routes.py
@@ -6,6 +6,8 @@ import random
 from flask import Flask, render_template, url_for, request, session, redirect, send_from_directory, flash, \
 jsonify
 
+app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
